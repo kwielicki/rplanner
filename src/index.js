e93c1e561a82e50e699e4a9d51f8074b458dce23
kwielicki <krzysztofwielicki86@gmail.com>
2020-03-21 19:23:16 +0100

    ServiceWorker changing directory
    Change-Id: Ic44b2608046f942f8d6c1438205d6830286e9e62
    Reviewed-on: https://review.gerrithub.io/c/kwielicki/rplanner/+/487691
    Reviewed-by: <krzysztofwielicki86@gmail.com>
    Tested-by: <krzysztofwielicki86@gmail.com>

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import { ThroughProvider } from 'react-through'
 import App from './components/app'
 import { BrowserRouter } from "react-router-dom"
-import * as serviceWorker from '../serviceWorker'
+import * as serviceWorker from './serviceWorker'
 import './styles/index.scss'
 import './config/env.js'
 
