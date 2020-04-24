865982e92e2ffebc47936c9e202a845c07d637d4
kwielicki <krzysztofwielicki86@gmail.com>
2020-04-24 08:58:31 +0200

    RPLANNER-11 Guest details - changing view
    Change-Id: I48cd8afe1c167ce724ce32d689d624067e932357

--- src/components/GuestTableManager/GuestTableDetails.js
+++ src/components/GuestTableManager/GuestTableDetails.js
@@ -11,27 +11,27 @@ function GuestTableDetails(props) {
     return (
         <div>
             <DecoratedList>
-                <DecoratedListElement header='Phone number' sizeDesktop='oneHalf'>
+                <DecoratedListElement header='Phone number' sizeDesktop='oneHalf' icon='phone-square' iconColor='green'>
                     {isEmpty(phoneNumber)
                         ? 'No phone number'
                         : <a href={`tel: ${phoneNumber}`}>{phoneNumber}</a>}
                 </DecoratedListElement>
-                <DecoratedListElement header='Email address' sizeDesktop='oneHalf'>
+                <DecoratedListElement header='Email address' sizeDesktop='oneHalf' icon='envelope-square' iconColor='orange'>
                     {isEmpty(emailAddress)
                         ? 'No email address'
                         : <a href={`mailto: ${emailAddress}`}>{emailAddress}</a>}
                 </DecoratedListElement>
-                <DecoratedListElement header='Children number'>
+                <DecoratedListElement header='Children number' sizeDesktop='oneHalf' icon='baby' iconColor='pink'>
                     {isEmpty(numberOfGuests.children)
                         ? 'No children'
                         : numberOfGuests.children}
                 </DecoratedListElement>
-                <DecoratedListElement header='Added data'>
+                <DecoratedListElement header='Added data' sizeDesktop='oneHalf' icon='calendar-day' iconColor='gray'>
                     {isEmpty(timestamp)
                         ? 'No added data'
                         : <Moment date={convertUtcDate(timestamp.seconds)} format="D MMM YYYY, HH:mm"/>}
                 </DecoratedListElement>
-                <DecoratedListElement header='Additional information' sizeDesktop='twoThirds'>
+                <DecoratedListElement header='Additional information' sizeDesktop='one' icon='info-circle'  iconColor='greenLight'>
                     {isEmpty(additionalInformation)
                         ? 'No additional information'
                         : additionalInformation}
