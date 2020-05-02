63cc1b426659faf5cd21cc8da81f1c91a9b13c4a
kwielicki <krzysztofwielicki86@gmail.com>
2020-05-02 18:29:56 +0200

    RPLANNER-14 Component Switcher
    Change-Id: I0051718e47daea590929a9e64d205f633753125a
    Reviewed-on: https://review.gerrithub.io/c/kwielicki/rplanner/+/491988
    Reviewed-by: <krzysztofwielicki86@gmail.com>
    Tested-by: <krzysztofwielicki86@gmail.com>

---
+++ src/components/UI/Switcher/Switcher.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import './Switcher.scss'
+
+function Switcher(props) {
+    const { checked, color, name, onChange, inputProps, disabled } = props
+
+    return (
+        <div styleName='Switcher'>
+            <div styleName='__inner'>
+                <span aria-disabled='false' styleName={classNames({
+                    '__button': true,
+                    '-checked': checked,
+                    '-disabled': disabled,
+                    [`-${color}`]: color
+                })}>
+                    <span styleName='__label'>
+                        <input
+                            type='checkbox'
+                            styleName='__input'
+                            name={name}
+                            onChange={onChange}
+                            checked={checked}
+                            disabled={disabled}
+                            {...inputProps}
+                        />
+                        <span styleName='__thumb'></span>
+                    </span>
+                </span>
+                <span styleName='__track'></span>
+            </div>
+        </div>
+    )
+}
+
+
+Switcher.propTypes = {
+    /**
+     * Used to toggle checked value
+     */
+    checked: PropTypes.bool,
+    /**
+     * Used to set color
+     */
+    color: PropTypes.oneOf(['primary', 'secondary']),
+    /**
+     * Used to set onChange action
+     */
+    onChange: PropTypes.func,
+    /**
+     * Used to set name attribute for input
+     */
+    name: PropTypes.string,
+    /**
+     * Used to set custom props for input
+     */
+    inputProps: PropTypes.object,
+    /**
+     * Used to disable state
+     */
+    disabled: PropTypes.bool,
+}
+
+
+export default Switcher
