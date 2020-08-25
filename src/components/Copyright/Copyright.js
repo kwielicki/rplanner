ace4a3c0b5109c59b8ebbd1fdd01b16f1e18141d
Krzysztof Wielicki <kwielicki@e-point.pl>
2020-08-25 13:18:44 +0200

    RPLANNER-26 Refactoring of Copyright component from Class to Function
    Change-Id: Id570c649ab194e13e36767c183df1e5f4fec5cfe
    Reviewed-on: https://review.gerrithub.io/c/kwielicki/rplanner/+/501224
    Reviewed-by: <krzysztofwielicki86@gmail.com>
    Tested-by: <krzysztofwielicki86@gmail.com>

--- src/components/Copyright/Copyright.js
+++ src/components/Copyright/Copyright.js
@@ -1,19 +1,17 @@
 import React from 'react'
-import CurrentYear from 'Components/CurrentYear';
+import CurrentYear from 'Components/CurrentYear'
+import translations from 'Translations/translations.json'
 import './Copyright.scss'
 
-class Copyright extends React.Component {
-
-    render() {
-        return (
-            <div styleName='Copyright'>
-                <div styleName='__inner'>
-                    {'\u00A9'}&nbsp;<CurrentYear/>.
-                    All rights reserved.
-                </div>
+export const Copyright = () => {
+    return (
+        <div styleName='Copyright'>
+            <div styleName='__inner'>
+                {'\u00A9'}&nbsp;<CurrentYear/>.
+                {translations.properties.allRightsReserved}
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Copyright
