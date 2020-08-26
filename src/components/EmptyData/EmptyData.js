fc2b077d97704d9678b8a84f1ef834ad171037b3
Krzysztof Wielicki <kwielicki@e-point.pl>
2020-08-26 18:10:34 +0200

    RPLANNER-27 Component "Empty Data"
    Change-Id: I6f803f84641d771218351e1ad67d44e04eb534d5

---
+++ src/components/EmptyData/EmptyData.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import LazyImage from 'Components/LazyImage'
+import EmptyImage from 'Assets/images/icons/inbox.svg'
+import translations from 'Translations/translations.json'
+import './EmptyData.scss'
+
+function EmptyData(props) {
+    const { icon, iconSize, title, subTitle } = props
+
+    return (
+        <div styleName='EmptyData'>
+            {icon && <div styleName='__icon'>
+                <div styleName='__iconSource'>
+                    <LazyImage src={EmptyImage}/>
+                </div>
+            </div>}
+            <div styleName='__title'>{title}</div>
+            {subTitle && <div styleName='__subtitle'>{subTitle}</div>}
+        </div>
+    )
+}
+EmptyData.propTypes = {
+    icon: PropTypes.bool,
+    iconSize: PropTypes.oneOf(['small', 'normal', 'large']),
+    title: PropTypes.string,
+    subTitle: PropTypes.string
+}
+
+EmptyData.defaultProps = {
+    title: translations.properties.noResultsFound
+}
+
+export default EmptyData
