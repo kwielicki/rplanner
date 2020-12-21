d12572bdd29d87ebe3e1a7f0e8f58a309f620b21
Krzysztof Wielicki <kwielicki@e-point.pl>
2020-12-21 14:57:44 +0100

    RPLANNER-37 Adding props "small" for Empty Data component && component refactor
    Change-Id: I44ee2d66d2e3e30bc0b2cb778ca174b4ad420a38
    Reviewed-on: https://review.gerrithub.io/c/kwielicki/rplanner/+/507972
    Reviewed-by: <krzysztofwielicki86@gmail.com>
    Tested-by: <krzysztofwielicki86@gmail.com>

--- src/components/EmptyData/EmptyData.js
+++ src/components/EmptyData/EmptyData.js
@@ -1,32 +1,55 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import SvgEmptyImage from 'Assets/images/icons/inbox.svg'
 import translations from 'Translations/translations.json'
 import './EmptyData.scss'
 
-function EmptyData(props) {
-    const { icon, iconSize, title, subTitle } = props
-
+function EmptyData({ icon, iconSize, title, subTitle, actionArea }) {
     return (
         <div styleName='EmptyData'>
-            {icon && <div styleName='__icon'>
-                <div styleName='__iconSource'>
+            {icon && <div styleName={classNames('__icon', {
+                [`-${iconSize}`]: iconSize
+            })}>
+                <div styleName={classNames('__iconSource', {
+                    [`-${iconSize}`]: iconSize
+                })}>
                     <SvgEmptyImage/>
                 </div>
             </div>}
             <div styleName='__title'>{title}</div>
             {subTitle && <div styleName='__subtitle'>{subTitle}</div>}
+            {actionArea && <div styleName='__actionArea'>{actionArea}</div>}
         </div>
     )
 }
 EmptyData.propTypes = {
+    /**
+     * Used to set icon.
+     */
     icon: PropTypes.bool,
+    /**
+     * Used to set icon size.
+     */
     iconSize: PropTypes.oneOf(['small', 'normal', 'large']),
+    /**
+     * Used to set title.
+     */
     title: PropTypes.string,
-    subTitle: PropTypes.string
+    /**
+     * Used to set subTitle.
+     */
+    subTitle: PropTypes.string,
+    /**
+     * Used to set action area.
+     */
+    actionArea: PropTypes.element
 }
 
 EmptyData.defaultProps = {
+    /**
+     * Used to set default title.
+     */
     title: translations.properties.noResultsFound
 }
 
