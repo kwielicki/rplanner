c9a080a3bb52aae5a65e1a02b5fe76277713975e
Krzysztof Wielicki <kwielicki@e-point.pl>
2021-01-25 22:43:12 +0100

    RPLANNER-45 Refactor of Header component from "class component" to "function component"
    Change-Id: I7c3d6732e13d8b53165f59917b699c0bfbcf4d69
    Reviewed-on: https://review.gerrithub.io/c/kwielicki/rplanner/+/509583
    Reviewed-by: <krzysztofwielicki86@gmail.com>
    Tested-by: <krzysztofwielicki86@gmail.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,46 +1,49 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import Container from 'Components/Grid/Container'
 import Column from 'Components/Grid/Column'
 import BreadcrumbsWrapper from 'Components/BreadcrumbsWrapper'
 import Logout from 'Components/Logout'
+import { AccountDropdown } from 'Components/AccountDropdown'
+import { ReminderDropdown } from 'Components/ReminderDropdown'
 import IconButton from 'Components/UI/IconButton'
+import { isEmpty } from 'lodash'
+import { useDispatch, useSelector } from "react-redux"
 import { hamburgerOpenAction } from 'Actions/hamburgerActions'
 import { overlayShowAction } from 'Actions/overlayActions'
 import './Header.scss'
 
-const mapStateToProps = state => {
-    return {
-		isOpen: state.hamburger.isOpen,
-		isOverlayActivated: state.overlay.isOverlayActivated
+export const Header = ( { isMobile } ) => {
+    const dispatch = useDispatch()
+    const isOpen = useSelector(state => state.hamburger.isOpen);
+    const isOverlayActivated = useSelector(state => state.overlay.isOverlayActivated);
+    const ownerData = useSelector(state => state.auth.ownerData);
+    const sideBarOpen = () => {
+        dispatch(hamburgerOpenAction(!isOpen))
+        dispatch(overlayShowAction(!isOverlayActivated))
     }
+    return (
+        <header styleName='Header'>
+            <Container>
+                <Column xs='1'>
+                    <div styleName='__inner'>
+                        <div styleName='__innerTop'>
+                            {!isEmpty(ownerData) &&
+                                <>
+                                    <ReminderDropdown/>
+                                    <AccountDropdown
+                                        user={ownerData}/>
+                                </>}
+                            <Logout/>
+                        </div>
+                        {isMobile &&
+                            <IconButton
+                                handleClick={() => sideBarOpen()}
+                                icon='bars'/>
+                        }
+                        <BreadcrumbsWrapper/>
+                    </div>
+                </Column>
+            </Container>
+        </header>
+    )
 }
-
-class Header extends React.Component {
-
-	sideBarOpen = () => {
-		this.props.dispatch(hamburgerOpenAction(!this.props.isOpen))
-		this.props.dispatch(overlayShowAction(!this.props.isOverlayActivated))
-	}
-
-	render() {
-		const { isMobile } = this.props
-		return (
-			<header styleName='Header'>
-				<Container>
-					<Column xs='1'>
-						<div styleName='__inner'>
-							<div styleName='__innerTop'>
-								<Logout/>
-							</div>
-							{isMobile && <IconButton handleClick={this.sideBarOpen} icon='bars'></IconButton> }
-							<BreadcrumbsWrapper/>
-						</div>
-					</Column>
-				</Container>
-			</header>
-		)
-	}
-}
-
-export default connect(mapStateToProps)(Header)
